dbe529269c66970f1f66d57a666a4cff136e9ee6
Mostakimw <scientist.expert@gmail.com>
2023-11-05 21:35:31 +0600

    typescript first module

---
+++ module1/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let course = "my love";
+console.log(course);
---
+++ module1/src/data-type.ts
@@ -0,0 +1,5 @@
+// array 
+let names: string[] = ['mosta', 'jhanker']
+
+// tuple >> array >> order >> types of values 
+let agaName: [number, string, boolean]=[50, 'Mr.X', true]
